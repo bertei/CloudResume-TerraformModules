6175b17dc2970c52fefec578d41a46a65348cbec
Bernardo <bteisceira@gmail.com>
2023-09-17 20:21:57 -0300

    Add: dynamodb item creation

--- dynamodb/main.tf
+++ dynamodb/main.tf
@@ -9,16 +9,13 @@ resource "aws_dynamodb_table" "main" {
   }
 }
 
-##Everytime I run tf plan, it restarts Quantity attribute to 0. Not good if i'm updating the 'view-counter'.
-##Probably i'll create the attribute manually.
-#resource "aws_dynamodb_table_item" "main" {
-#  table_name = aws_dynamodb_table.main.name
-#  hash_key   = aws_dynamodb_table.main.hash_key
-#
-#  item = <<ITEM
-#{
-#  "Stat": {"S": "view-count"},
-#  "Quantity": {"N": "0"}
-#}
-#ITEM
-#}
+resource "aws_dynamodb_table_item" "main" {
+  table_name = aws_dynamodb_table.main.name
+  hash_key   = aws_dynamodb_table.main.hash_key
+
+  item = <<ITEM
+{
+  "Stat": {"S": "view-count"}
+}
+ITEM
+}
